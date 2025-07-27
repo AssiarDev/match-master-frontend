b8df9c00f3918c152ffd5cf9c4c902d3a48319f9
AssiarDev <halibaraissa@gmail.com>
2025-07-27 13:00:55 +0200

    [ADD] création d'un hook pour que l'utilisateur ajoute à ses favoris l'équipe qu'il selectionne

---
+++ src/hooks/useAddFavorite.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const useAddFavorite = () => {
+    const [error, setError] = useState(null);
+
+    const addFavorite = async (userId, clubId) => {
+        const id = parseInt(clubId)
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/protected/users/favorites`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                credentials: "include",
+                body: JSON.stringify({userId, id})
+            });
+
+            if(!response.ok){
+                throw new Error('Erreur lors de la reponse.')
+            };
+
+            const data = await response.json();
+            return data;
+
+        } catch (e){
+            setError("Une erreur est survenue : ", e)
+        }
+    }
+
+    return { addFavorite, error }
+    
+}
