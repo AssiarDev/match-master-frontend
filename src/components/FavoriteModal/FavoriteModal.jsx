545e2e8f56d7656bf489e054d23527d7183d17b3
AssiarDev <halibaraissa@gmail.com>
2025-08-16 12:32:59 +0200

    [ADD] button X pour pouvoir supprimer un favoris dans via le modal

--- src/components/FavoriteModal/FavoriteModal.jsx
+++ src/components/FavoriteModal/FavoriteModal.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react";
 import { Link, useNavigate } from "react-router";
 import { useFavorite } from "../../hooks/useFavorite";
+import { useDeleteFavorite } from "../../hooks/useDeleteFavorite";
 
 export const FavoriteModal = () => {
     const [open, setOpen] = useState(true);
     const navigate = useNavigate();
-    const { favorite, error } = useFavorite();
+    const { favorite, error, refreshFavorites } = useFavorite();
+    const { deleteFavorite } = useDeleteFavorite()
 
     const hasError = Boolean(error);
+
+    const handleDelete = async (clubId) => {
+        const result = await deleteFavorite(clubId);
+
+        if(result?.message){
+            refreshFavorites();
+        }
+    }
 
     return (
         <Dialog open={open} onClose={() => setOpen(false)} className="relative z-50">
@@ -32,10 +42,14 @@ export const FavoriteModal = () => {
                 <div>
                     {hasError ? <p className="text-red-500 text-sm text-center">{error}</p> : favorite.map((fav, index) => {
                         return (
-                            <div key={index} className="flex items-center gap-2 border-white border-b">
+                            <div key={index} className="flex justify-center items-center gap-2 border-white border-b mb-2">
                                 <img src={fav.emblem} alt={fav.name} className="mb-2 h-5 w-5"/>
                                 <Link className="mb-2 text-white hover:underline">{fav.name}</Link>
-                                {/* <p className="mb-2 text-white">{fav.name}</p> */}
+                                <button
+                                    onClick={() => handleDelete(fav.id)} className="ml-auto mr-2 cursor-pointer text-white hover:text-red-500"
+                                >
+                                    x
+                                </button>
                             </div>
                         )
                     })}
