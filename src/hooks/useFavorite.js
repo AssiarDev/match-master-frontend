89fe155c3a55b15bd81236e4249d2a680ca122ed
AssiarDev <halibaraissa@gmail.com>
2025-08-16 12:32:05 +0200

    [FIX] ajout de la fonction refreshFavorites pour que les favoris s'actualisent directement au moment de l'ajout ou de a supression d'un favoris

--- src/hooks/useFavorite.js
+++ src/hooks/useFavorite.js
@@ -5,28 +5,30 @@ export const useFavorite = () => {
     const [favorite, setFavorite] = useState([]);
     const { user } = useAuth();
     const [error, setError] = useState(null);
+
+    const fetchFavorite = async () => {
+
+        if (!user?.id) return;
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/protected/users/${user.id}/favorites`, {
+            credentials: "include"
+        });
+
+        if(!response.ok){
+            throw new Error('Erreur lors de la reponse.')
+        };
+
+        const data = await response.json();
+        setFavorite(data);
+
+        } catch (e){
+            setError(e.message)
+        }
+    }
     
     useEffect(() => {
-        if (!user?.id) return;
-
-        const fetchFavorite = async () => {
-            try {
-                const response = await fetch(`${import.meta.env.VITE_API_URL}/protected/users/${user.id}/favorites`, {
-                    credentials: "include"
-                });
-                if(!response.ok){
-                    throw new Error('Erreur lors de la reponse.')
-                };
-
-                const data = await response.json();
-                setFavorite(data);
-
-            } catch (e){
-                setError(e.message)
-            }
-        }
         fetchFavorite();
     },[user]);
 
-    return { favorite, error}
+    return { favorite, error, refreshFavorites: fetchFavorite }
 }
